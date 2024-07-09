cf4fb321675cae694beedcc4faefccab2a5f67a9
Yoshi Rubin <yoshrubin@gmail.com>
2024-07-09 11:41:09 +0300

    fix up nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center bg-slate-400 p-8 shadow-md">
+    <div className="flex items-center bg-slate-400 p-8 shadow-md justify-between">
+      <h1 className="text-2xl font-bold">🍺 BreweryDB</h1>
       <nav className="flex gap-4">
         <Link className="hover:underline" to="/">
           Browse
         </Link>
-        <Link className="hover:underline " to="/favorites">
+        <Link className="hover:underline" to="/favorites">
           Favorites
         </Link>
       </nav>
-      <h1 className="text-2xl font-bold">BreweryDB</h1>
     </div>
   );
 }
